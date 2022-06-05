13643d12567c330916be7f75704e8232a5ef9091
saleemAbed99 <saleemabed15@gmail.com>
2022-06-06 01:02:36 +0300

    Section one

---
+++ Entities/AppUser.cs
@@ -0,0 +1,14 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
+
+// What is Entity Framework?
+// Entity Framework is a .NET ORM (Object Relational Mapping) library 
+// allows you to map your object-oriented data model to a relational database.
+// translate our code into SQL commands and execute them against a relational database.
+
